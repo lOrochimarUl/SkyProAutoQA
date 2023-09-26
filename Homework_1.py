1a5458b431745187d887699ae30be5048f7d5543
Byk Alexandr <bik.san1357911@gmail.com>
2023-09-26 19:02:37 +0300

    Add first homework

---
+++ Homework_1.py
@@ -0,0 +1,72 @@
+my_name = "ALex"
+print(my_name)
+
+my_age = 25
+my_age = my_age + 3
+print(my_age)
+
+print("Please, enter your name")
+username = input()
+print("Hello, " + username)
+
+
+print("Please, enter your first name")
+first_name = input()
+print("Please, enter your last name")
+last_name = input()
+print("Your name is " + first_name + " " + last_name)
+
+def print_greeting():
+    print("Hello World!")
+print_greeting()
+
+def print_1():
+    print(1)
+def print_2():
+    print(2)
+def print_3():
+    print(3)
+def print_4():
+    print(4)
+def print_5():
+    print(5)
+def print_6():
+    print(6)
+def print_7():
+    print(7)
+def print_8():
+    print(8)
+def print_9():
+    print(9)
+def print_0():
+    print(0)
+
+print_8()
+print_8()
+print_0()
+print_0()
+print_5()
+print_5()
+print_5()
+print_3()
+print_5()
+print_3()
+print_5()
+
+def print_something(num):
+    print(num)
+
+print_something(8)
+print_something(8)
+print_something(0)
+print_something(0)
+print_something(5)
+print_something(5)
+print_something(5)
+print_something(3)
+print_something(5)
+print_something(3)
+print_something(5)
+
+
+
